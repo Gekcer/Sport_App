316e4e9e844f64b8b4981e515b3218c725fd7778
Gekcer <gekcer@gmail.com>
2023-09-21 12:01:19 +0300

    Вынес класс кнопок к тем экранам, на которых они находятся, добавил переключение на экран создания тренировки

--- creating_training_screen.py
+++ creating_training_screen.py
@@ -1,6 +1,27 @@
+from kivy.app import App
 from kivy.uix.screenmanager import Screen
+from kivy.uix.button import Button
 
 class CreatingTrainingScreen(Screen):
     def __init__(self, **kwargs):
         super(CreatingTrainingScreen, self).__init__(**kwargs)
+        self.orientation = "vertical"
         
+        self.back_button = GoBackButton(on_release = self.go_back)
+        
+        self.add_widget(self.back_button)
+        
+    def go_back(self, instance):
+        screen_manager = App.get_running_app().root
+        screen_manager.transition.direction = 'right'
+        screen_manager.current = "Главный экран"
+    
+class GoBackButton(Button):
+    def __init__(self, **kwargs):
+        super(GoBackButton, self).__init__(**kwargs)
+        self.text = "Назад"
+        self.font_size = 12
+        self.background_color = [1, 0.976, 0, 1]
+        self.size_hint = (None, None)
+        self.size = (150, 50)
+        self.pos_hint = {"center_x": 0.5, "center_y": 0.9}
--- main_screen.py
+++ main_screen.py
@@ -1,22 +1,46 @@
+from kivy.app import App
 from kivy.uix.screenmanager import Screen
 from kivy.graphics import Color, Rectangle
+from kivy.uix.button import Button
 
-from create_training_button import CreateTrainingButton
-from choose_training_button import ChooseTrainingButton
+from creating_training_screen import CreatingTrainingScreen
 
 class MainScreen(Screen):
     def __init__(self, **kwargs):
         super(MainScreen, self).__init__(**kwargs)
         self.orientation = "vertical"
         
-        create_training_button = CreateTrainingButton()
-        choose_training_button = ChooseTrainingButton()
+        self.create_training_button = CreateTrainingButton(on_release = self.open_create_training_screen)
+        self.choose_training_button = ChooseTrainingButton()
         
-        self.add_widget(create_training_button)
-        self.add_widget(choose_training_button)
+        self.add_widget(self.create_training_button)
+        self.add_widget(self.choose_training_button)
         
-        # print(dir(self.canvas))
-        
+    def open_create_training_screen(self, instance):
+        screen_manager = App.get_running_app().root
+        screen_manager.transition.direction = 'left'
+        screen_manager.current = "Создание тренировки"
+
+class CreateTrainingButton(Button):
+    def __init__(self, **kwargs):
+        super(CreateTrainingButton, self).__init__(**kwargs)
+        self.text = "Создать тренировку"
+        self.font_size = 12
+        self.background_color = [1, 0.976, 0, 1]
+        self.size_hint = (None, None)
+        self.size = (150, 50)
+        self.pos_hint = {"center_x": 0.5, "center_y": 0.9}
+
+class ChooseTrainingButton(Button):
+    def __init__(self, **kwargs):
+        super(ChooseTrainingButton, self).__init__(**kwargs)
+        self.text = "Выбрать тренировку"
+        self.font_size = 12
+        self.background_color = [1, 0.976, 0, 1]
+        self.size_hint = (None, None)
+        self.size = (150, 50)
+        self.pos_hint = {"center_x": 0.5, "center_y": 0.8}
+
 if __name__ == "__main__":
     scr = MainScreen()
     print(dir(scr))
